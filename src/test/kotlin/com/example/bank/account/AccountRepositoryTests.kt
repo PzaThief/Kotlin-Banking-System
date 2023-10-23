01cf12306f60f78bea470b273dd331283594b2f4
PzaThief <vwjdalsgkv@gmail.com>
2023-10-23 22:36:58 +0900

    add jpaAudit test code and checked it success

--- src/test/kotlin/com/example/bank/account/AccountRepositoryTests.kt
+++ src/test/kotlin/com/example/bank/account/AccountRepositoryTests.kt
@@ -1,9 +1,13 @@
 package com.example.bank.account
 
+import com.example.bank.account.domain.Account
+import com.example.bank.account.domain.AccountProduct
 import com.example.bank.account.infrastructure.jpa.AccountJpaRepository
+import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import java.math.BigDecimal
 
 @SpringBootTest
 class AccountRepositoryTests(
@@ -16,5 +20,23 @@ class AccountRepositoryTests(
         val firstAccountId = accountRepository.nextAccountId().also { println(it) }
         val secondAccountId = accountRepository.nextAccountId().also { println(it) }
         assert(firstAccountId.value < secondAccountId.value)
+    }
+
+    @Test
+    fun jpaAuditShouldGenerateCreatedAtAndUpdatedAt() {
+        val account = accountRepository.saveAndFlush(
+            Account(
+                id = Account.Id(1),
+                displayId = Account.DisplayId("1111-0000001"),
+                ownerName = "홍길동",
+                accountProduct = AccountProduct("1111"),
+                initialDeposit = BigDecimal(0),
+            )
+        )
+        assertNotNull(account.createdAt)
+
+        account.balance = BigDecimal(100)
+        accountRepository.saveAndFlush(account)
+        assertNotNull(account.updatedAt)
     }
 }
