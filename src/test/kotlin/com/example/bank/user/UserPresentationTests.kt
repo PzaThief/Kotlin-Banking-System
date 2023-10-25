5e487cfa64a3410ba2665dae89fcfdaf67d408ee
PzaThief <vwjdalsgkv@gmail.com>
2023-10-26 00:59:09 +0900

    add test code for user presentation

---
+++ src/test/kotlin/com/example/bank/user/UserPresentationTests.kt
@@ -0,0 +1,66 @@
+package com.example.bank.user
+
+import com.example.bank.user.application.UserCreateRequest
+import com.example.bank.user.application.UserResponse
+import kotlinx.coroutines.runBlocking
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Nested
+import org.mockito.Mockito
+import org.mockito.kotlin.whenever
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.http.MediaType
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.web.reactive.server.WebTestClient
+import org.springframework.web.reactive.function.BodyInserters
+import java.time.LocalDateTime
+
+@WebFluxTest(controllers = [UserController::class])
+@ActiveProfiles("test")
+class UserPresentationTests(
+    @Autowired
+    val webClient: WebTestClient,
+) {
+    @MockBean
+    lateinit var application: UserApplication
+
+    @Nested
+    inner class CreateUser {
+        fun createAccountShouldReturnDtoWith200() {
+            runBlocking {
+                val userCreateRequest = UserCreateRequest(
+                    loginId = "kotlin",
+                    loginPassword = "bank",
+                    name = "홍길동",
+                    personCategory = "natural",
+                    registrationNumber = "990123-1012345",
+                )
+                val userCreateResponse = UserResponse(
+                    Id = 1,
+                    loginId = "kotlin",
+                    name = "홍길동",
+                    personCategory = "natural",
+                    updatedAt = LocalDateTime.now(),
+                    createdAt = LocalDateTime.now(),
+                )
+
+                whenever(application.createUser(userCreateRequest)).thenReturn(userCreateResponse)
+                webClient.post()
+                    .uri("/user/")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(BodyInserters.fromValue(userCreateRequest))
+                    .exchange()
+                    .expectStatus().isOk()
+                    .expectBody(UserResponse::class.java)
+                    .consumeWith {
+                        Assertions.assertThat(it.responseBody).isNotNull()
+                        Assertions.assertThat(it.responseBody!!.id).isNotNull()
+                        Assertions.assertThat(it.responseBody!!.createdAt).isNotNull()
+                    }
+
+                Mockito.verify(application, Mockito.times(1)).createUser(userCreateRequest)
+            }
+        }
+    }
+}
